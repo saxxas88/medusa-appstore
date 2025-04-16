db6d5dd0b494e040133008a64c795c5d7291214b
Salvatore Basile <saxxas88@gmail.com>
2025-04-16 16:45:38 +0200

    Update medusa-config

--- medusa-config.ts
+++ medusa-config.ts
@@ -47,15 +47,17 @@ module.exports = defineConfig({
       resolve: "@medusajs/medusa/file",
       options: {
         providers: [
-          {
-            resolve: `medusa-file-cloudinary`,
-            options: {
-                cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-                api_key: process.env.CLOUDINARY_API_KEY,
-                api_secret: process.env.CLOUDINARY_API_SECRET,
-                secure: true,
-            },
-        },
+         
+            {
+              resolve: `medusa-file-cloudinary`,
+              id: "s3",
+              options: {
+                  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                  api_key: process.env.CLOUDINARY_API_KEY,
+                  api_secret: process.env.CLOUDINARY_API_SECRET,
+                  secure: true,
+              },
+          }
         ],
       },
     },
